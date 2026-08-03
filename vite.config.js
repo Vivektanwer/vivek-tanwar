80810f262f640fa370b25f27bc01a4804908b4d2
Vivek Tanwar <vivektanwar648@gmail.com>
2026-08-03 13:41:25 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-  tailwindcss(),],
+    tailwindcss(),
+  ],
+  ssr: {
+    // React aur baaki dependencies ko bundle kar do taaki Vercel pe external import na ho
+    noExternal: ['react', 'react-dom', 'react-router', 'react-router-dom'],
+  },
 })
