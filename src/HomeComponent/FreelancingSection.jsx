4ff5962374e6c1b8a53733444083ed3b9febbf4f
Vivek Tanwar <vivektanwer648@gmail.com>
2026-08-05 16:51:35 +0530

    Revise freelancing section text
    Updated the freelancing section text to emphasize collaboration and pride in work.

--- src/HomeComponent/FreelancingSection.jsx
+++ src/HomeComponent/FreelancingSection.jsx
@@ -8,8 +8,8 @@ const FreelancingSection = () => {
         style={{ backgroundImage: `url('/bg-image-68d543cee40f4.webp')`, backgroundSize: 'cover', backgroundPosition: 'center' }}
         >
         <div className='max-w-7xl mx-auto px-4 py-20 sm:p-8 md:p-45 text-center'>
-        <p className='text-4xl second_Text_Color font-semibold'><strong>I'm<span className='third_Text_Color'> Available </span>for freelancing</strong></p>
-        <p className='text-gray-100 max-w-xl mx-auto mt-4'>I am available for freelancing, offering custom web development solutions tailored to your needs. Let’s work together to build fast, responsive, and user-friendly websites!</p>
+        <p className='text-4xl second_Text_Color font-semibold'><strong>Let's build <span className='third_Text_Color'> something </span>we're both proud of.</strong></p>
+        {/* <p className='text-gray-100 max-w-xl mx-auto mt-4'>I am available for freelancing, offering custom web development solutions tailored to your needs. Let’s work together to build fast, responsive, and user-friendly websites!</p> */}
         </div>
         </div>
     </>
